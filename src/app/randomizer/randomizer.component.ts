977453891ff3f2f9ba29f32c98a73a9695dcda44
Neil Kalanish <Neilkal867@icloud.com>
2020-05-17 20:11:18 -0400

    update

--- src/app/randomizer/randomizer.component.ts
+++ src/app/randomizer/randomizer.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-randomizer',
   templateUrl: './randomizer.component.html',
@@ -8,33 +6,13 @@ import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@ang
 })
 export class RandomizerComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      checkArray: this.fb.array([])
-    });
-  }
+  constructor() {}
+  title = 'THE TEAM OLYMPUS STAND UP RANDOMIZER';
+  button = 'ClICK ME TO SEE WHO GOES NEXT';
 
   whoGoesNext: string;
-  arrayLength: number;
-  form: FormGroup;
+  arrayLength: number;  
   olympusTeam: string[] = [ 'Aaron' , 'Nathan', 'Henry', 'Neil', 'Nabih', 'Dylan', 'George', 'Keith', 'Sebastian', 'Adam', 'Rox'];
-  //  olympusTeam: string[];
-
-  Data: Array<any> = [
-    { name: 'Aaron', value: 'Aaron' },
-    { name: 'Nathan', value: 'Nathan' },
-    { name: 'Henry', value: 'Henry' },
-    { name: 'Neil',  value: 'Neil' },
-    { name: 'Nabih', value: 'Nabih' },
-    { name: 'Dylan', value: 'Dylan' },
-    { name: 'George', value: 'George' },
-    { name: 'Keith', value: 'Keith' },
-    { name: 'Sebastian', value: 'Sebastian' },
-    { name: 'Adam', value: 'Adam' },
-    { name: 'Rox', value: 'Rox' },
-  ];
-
-
 
   ngOnInit() { }
 
@@ -49,27 +27,4 @@ export class RandomizerComponent implements OnInit {
       this.whoGoesNext = 'That is everyone, have a good rest of your day!';
     }
   }
-
-
-  onCheckboxChange(e) {
-    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
-
-    if (e.target.checked) {
-      checkArray.push(new FormControl(e.target.value));
-    } else {
-      let i = 0;
-      checkArray.controls.forEach((item: FormControl) => {
-        if (item.value === e.target.value) {
-          checkArray.removeAt(i);
-          return;
-        }
-        i++;
-      });
-    }
-  }
-
-  submitForm() {
-    console.log(this.form.value);
-  }
-
 }
